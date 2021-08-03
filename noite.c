42fc82cf7dfbfa8f7b24b7d7bf589cc65431acbc
Przysiada <przysiada_13@hotmail.com>
2021-08-03 20:55:58 -0300

    Update noite.c
    Przysiada change

--- noite.c
+++ noite.c
@@ -1,7 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct elemento
+{
+  char nome[100];
+  int vale;
+  int planoSaude;
+  float salario;
+  float gratificacao;
+  int dependentes;
+} funcionario;
+
+
 void main()
 {
-  printf("Teste");
+  char op = '0';
+  printf("Bem vindo ao sistema\n");
+
+  while(op != 'X')
+  {
+    scanf("%c", &op);
+    switch (op)
+    {
+    case 'A':
+    //Alguma coisa
+      break;
+    
+    default:
+      printf("ERRO : Voce escreveu errado usuario burro\n");
+      break;
+    }
+    if (op == 'A')
+    else if (op == 'B')
+    else if ()
+  }
 }
