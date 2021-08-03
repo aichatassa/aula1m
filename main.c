7b51f3eaa085c98a262f41e9cf5f357943bae54a
Przysiada <przysiada_13@hotmail.com>
2021-08-03 10:49:34 -0300

    Update main.c
    1

--- main.c
+++ main.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
+
+struct funcionario
+{
+    char nome;
+    int vale;
+    int planoSaude;
+    float salario;
+    int dependentes;
+};
+
 
 void main()
 {
-    printf("Teste");
+
 }
