6925aef3899013d8b3386bb02c09c3d739866e7c
MuriloLaba <84825163+MuriloLaba@users.noreply.github.com>
2021-08-03 11:10:55 -0300

    Mundaça Murilo

--- main.c
+++ main.c
@@ -2,17 +2,31 @@
 #include <stdlib.h>
 #include <locale.h>
 
-struct funcionario
+typedef struct 
 {
-    char nome;
+    char nome[100];
     int vale;
     int planoSaude;
     float salario;
     int dependentes;
-};
+} funcionario;
 
+funcionario *lista;
 
 void main()
 {
-
+	char nome[100];
+	int qntFuncionario, i=0;
+    funcionario func;
+    func = (funcionario) malloc (sizeof (funcionario));
+    printf("\n\tOlá, seja bem-vindo a folha de pagamento!\n");
+    printf("\nQuantos funcionarios?");
+    scanf("%d", &qntFuncionario);
+	for(i; i<qntFuncionario; i++){
+		printf("\n\tEntre com seu nome: ");
+	    scanf("%c", nome);
+	    printf("\nPossui vale transporte? \n(1-Sim\n0-Nao)");
+	    
+	    
+	}
 }
