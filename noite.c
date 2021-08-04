fc578cfeb5301f820461fdc1c823debe20eb924d
aichatassa <57256843+aichatassa@users.noreply.github.com>
2021-08-03 21:08:58 -0300

    aixas change

--- noite.c
+++ noite.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define OP_SAIR 'X' 
 
 typedef struct elemento
 {
@@ -17,21 +18,35 @@ void main()
   char op = '0';
   printf("Bem vindo ao sistema\n");
 
-  while(op != 'X')
+  while(op != OP_SAIR)
   {
+
     scanf("%c", &op);
     switch (op)
     {
     case 'A':
-    //Alguma coisa
+      versalario();
       break;
+    case
     
     default:
       printf("ERRO : Voce escreveu errado usuario burro\n");
       break;
     }
-    if (op == 'A')
-    else if (op == 'B')
-    else if ()
-  }
+  
+}
+
+void menu(){
+  const 
+  printf("Cadastrar funcionario\n");
+  printf("Imprimir funcionarios\n");
+  printf("Calcular VT\n");
+  printf("Calcular plano de saúde\n");
+  printf("Calcular o FGTS\n");
+  printf("Calcular o INSS\n");
+  printf("Calcular o salario familia\n");
+  printf("Calcular o IR\n");
+  printf("Calcular o Salario liquido");
+  printf("X - Sair");
+
 }
